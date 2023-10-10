5b480460b7ec9d72c175dae4941e2c3cae6a4432
jacksonz16 <jacksonz2390@gmail.com>
2023-10-10 09:29:24 -0400

    1

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("connected")
+const human = [{
+    name:"herlock sholmes",
+    job:"detective",
+    nationality:"Yorkshire tea"
+}]
+const human = [{
+    name:"Momes Jariarty",
+    job:"English professor",
+    nationality:"Yorkshire tea"
+}]
