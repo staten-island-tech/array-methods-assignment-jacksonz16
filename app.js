e3f109ce7a2cea6bf65d1a543ddf30f2c6f759f5
jacksonz16 <jacksonz2390@gmail.com>
2023-10-11 09:28:51 -0400

    2

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ console.log("connected")
 const human = [{
     name:"herlock sholmes",
     job:"mediocre detective",
-    nationality:"Yorkshire tea"
+    nationality:"Yorkshire tea",
+genetic_breakdown: ["80% men","4.2% women","6.9% dog","8.9% besian"]
 }]
  [{
     name:"Momes Jariarty",
     job:"Emoiji translation professor",
-    nationality:"New york pizza"
+    nationality:"Kenyian  Single origin light roasted coffee bean with a 4:6 ratio coffee served at 205F",
+genetic_breakdown: ["90% cringe","4.2% women","6.9% dog","8.9% besian"]
 }]
 [{
     name:"Jaun Jatson",
     job:"mexcian professor",
-    nationality:"New york pizza"
+    nationality:"detiroit pizza"
+}]
+[{
+    name:"arsène lupin ",
+    job:"homeless looter",
+    nationality:"J'aime des baguettes"
 }]
