29faaa90f5d7fe7ac0d0789d6e4bbaf597a34a19
jacksonz16 <jacksonz2390@gmail.com>
2023-10-12 09:29:19 -0400

    5

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ const human = [{
     name:"herlock sholmes",
     job:"mediocre detective",
     nationality:"Yorkshire tea",
-genetic_breakdown: ["80% men","4.2% women","6.9% dog","8.9% besian"]
+    personality: ["80% men","4.2% women","6.9% dog","8.9% besian"]
 }]
  [{
     name:"Momes Jariarty",
     job:"Emoiji translation professor",
     nationality:"Kenyian  Single origin light roasted coffee bean with a 4:6 ratio coffee served at 205F",
-genetic_breakdown: ["90% cringe","4.2% women","6.9% dog","8.9% besian"]
+    personality: ["90% cringe","3% socks with sandles","6.9% PEPE","0.1% kek"]
 }]
 [{
     name:"Jaun Jatson",
-    job:"mexcian professor",
-    nationality:"detiroit pizza"
+    job:"detroit urban survival training professor",
+    nationality:"detiroit pineapple pizza",
+    personality: ["10% mexican ","80% itlian","10% dead inside","100% indebt","100% getting sued"]
 }]
 [{
     name:"arsène lupin ",
     job:"homeless looter",
-    nationality:"J'aime des baguettes"
+    nationality:"J'aime des baguettes",
+    personality: ["50% moist looter","25% dishonest acts at a fair and reasonable price!","25% Britsh disaprover"]
 }]
