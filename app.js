941f96c43525d064b6ed833bd63e2fe0fc4e6e58
jacksonz16 <jacksonz2390@gmail.com>
2023-10-13 09:27:42 -0400

    git guod

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const human = [{
     job:"mediocre detective",
     nationality:"Yorkshire tea",
     personality: ["80% men","4.2% women","6.9% dog","8.9% besian"]
-}]
- [{
+},
+{
     name:"Momes Jariarty",
     job:"Emoiji translation professor",
     nationality:"Kenyian  Single origin light roasted coffee bean with a 4:6 ratio coffee served at 205F",
     personality: ["90% cringe","3% socks with sandles","6.9% PEPE","0.1% kek"]
-}]
-[{
+},
+{
     name:"Jaun Jatson",
     job:"detroit urban survival training professor",
     nationality:"detiroit pineapple pizza",
-    personality: ["10% mexican ","80% itlian","10% dead inside","100% indebt","100% getting sued"]
-}]
-[{
+    personality: ["10% mexican ","80% itlian","10% dead inside","100% getting sued","100% indebt"]
+},
+{
     name:"arsène lupin ",
     job:"homeless looter",
     nationality:"J'aime des baguettes",
     personality: ["50% moist looter","25% dishonest acts at a fair and reasonable price!","25% Britsh disaprover"]
 }]
+
+human.forEach((human) => console.log(human.name));
+human.forEach((human) => console.log(human.personality));
